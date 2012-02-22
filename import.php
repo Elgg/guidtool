1f0033c8775c64ba30e32f145d364c2c1db689e3
Steve Clay <steve@mrclay.org>
2012-02-21 21:26:31 -0500

    modifications for 1.8

--- import.php
+++ import.php
@@ -1,25 +1,22 @@
 <?php
-	/**
-	 * Elgg GUID Tool
-	 * 
-	 * @package ElggGUIDTool
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider Ltd
-	 * @copyright Curverider Ltd 2008-2010
-	 * @link http://elgg.com/
-	 */
+/**
+ * Elgg GUID Tool
+ *
+ * @package ElggGUIDTool
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Curverider Ltd
+ * @copyright Curverider Ltd 2008-2010
+ * @link http://elgg.com/
+ */
 
-	admin_gatekeeper();
-	set_context('admin');
-	
-	$format = get_input('format', 'opendd');
+admin_gatekeeper();
+elgg_set_context('admin');
 
-	// Render the file upload page
-	$title = elgg_echo("guidtool:import");
-	$body = elgg_view_title($title);
-	$body .= elgg_view("forms/guidtool/import", array('format' => $format, 'forward_url'));
-	
-	$body = elgg_view_layout('two_column_left_sidebar', '', $body);
-	
-	page_draw($title, $body);
-?>
+$format = get_input('format', 'opendd');
+
+// Render the file upload page
+$title = elgg_echo("guidtool:import");
+$body = elgg_view_title($title);
+$body .= elgg_view("forms/guidtool/import", array('format' => $format, 'forward_url'));
+
+echo elgg_view_page($title, elgg_view_layout("two_column_left_sidebar", array('area2' => $body)));
